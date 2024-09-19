2aa89b3e3d339050bc0e557c8c160a93ae23d97a
Taylor Burgess <tayloraburgess@users.noreply.github.com>
2024-09-18 20:17:12 -0400

    update

--- strip-silence.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-for f in `find "${1}" -name "*.${2}" -type f`; do
-    sox "${f%.*}.${2}" "${f%.*}-s.${2}" silence 1 0.1 0.01% reverse silence 1 0.1 0.01% reverse
-done
--- to-flac.sh
+++ to-flac.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 for f in `find "${1}" -name "*.${2}" -type f`; do
-    sox "${f%.*}.${2}" -C 8 "${f%.*}.flac" norm -0.1
+    sox "${f%.*}.${2}" -C 8 "${f%.*}-r.flac" norm 0
 done
